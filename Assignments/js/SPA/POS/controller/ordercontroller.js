4f28d1653d1ef8154db762e74199875d52254b18
PoornaShakthi13 <shakthihw13@gmail.com>
2022-10-14 17:56:00 +0530

    index updated

--- Assignments/js/SPA/POS/controller/ordercontroller.js
+++ Assignments/js/SPA/POS/controller/ordercontroller.js
@@ -1,3 +1,14 @@
 $("#txtOrderCustomerID").click(function () {
-
+        // alert("xsadcds");
+       loadAllCustomerID();
 })
+
+function loadAllCustomerID() {
+    // $("#txtOrderCustomerID").click(function () {
+        $("#txtOrderCustomerID").empty();
+        $("#txtOrderCustomerID").append(`<option></option>`);
+        for (let cus of customerArr){
+            $("#txtOrderCustomerID").append(`<option>${cus.cid}</option>`);
+        }
+    // })
+}
