037977408be0e0988dec6af4a58971f3b513679e
PoornaShakthi13 <shakthihw13@gmail.com>
2022-11-08 14:38:25 +0530

    index updated

--- Assignments/js/in class assignments/task 02/js/mainJS.js
+++ Assignments/js/in class assignments/task 02/js/mainJS.js
@@ -13,8 +13,17 @@ var queueDetails={
 function setDueData() {
     $("#content").empty();
     for (let i = 0; i < queueDetails.names.length ; i++) {
-        $("#content") .append(`<div style="background-color: ${queueDetails.names[i].color}">
-        <h1>${queueDetails.names[i].name}</h1>
-         </div>`)
+        $("#content").append(`<div style="background-color: ${queueDetails.names[i].color}"><h1>${queueDetails.names[i].name}</h1></div>`)
     }
+
+    queueDetails.nextName();
 }
+
+setInterval(setDueData,500);
+
+$("#content").click(function () {
+    var pName=$("txtName").val();
+    var pColor=$("#colors").val();
+    var person={name:pName, color:pColor};
+    queueDetails.names.unshift(person)
+})
