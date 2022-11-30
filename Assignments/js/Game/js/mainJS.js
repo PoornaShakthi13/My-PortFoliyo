06873048855b6034961b274330bc25ea0e52dd04
PoornaShakthi13 <shakthihw13@gmail.com>
2022-11-30 16:08:11 +0530

    index updated

---
+++ Assignments/js/Game/js/mainJS.js
@@ -0,0 +1,29 @@
+var boy = document.getElementById("boy");
+
+
+var IdeleImage=1;
+var IdeleAnimation=0;
+
+
+function idelAnimation() {
+        IdeleImage=IdeleImage+1;
+
+
+        if (IdeleImage===11){
+
+            IdeleImage=1;
+        }
+
+        boy.src="assest/idle (" + IdeleImage + ").png";
+}
+
+function ideleAnimationStart() {
+            idelAnimation=setInterval(idelAnimation,"200");
+
+}
+
+
+function keyCheck() {
+
+}
+
