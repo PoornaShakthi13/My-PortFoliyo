c725d1aaabd267da7078891a9e5b78df1a182f72
PoornaShakthi13 <shakthihw13@gmail.com>
2022-11-30 22:01:38 +0530

    index updated

--- Assignments/js/Game/js/mainJS.js
+++ Assignments/js/Game/js/mainJS.js
@@ -3,6 +3,8 @@ var boy = document.getElementById("boy");
 
 var IdeleImage = 1;
 var IdeleAnimation = 0;
+var runImageNumber = 1;
+var runAnimationNumber = 0;
 
 
 function idelAnimation() {
@@ -11,16 +13,29 @@ function idelAnimation() {
     if (IdeleImage === 11) {
         IdeleImage = 1;
     }
-    boy.src = "assest/idle (" + IdeleImage + ").png";
+    boy.src = "assest/idle(" + IdeleImage + ").png";
 }
 
 function ideleAnimationStart() {
-    idelAnimation = setInterval(idelAnimation, "500");
+    idelAnimation = setInterval(idelAnimation, "200");
 
 }
 
+function runAnimation() {
+    runImageNumber = runImageNumber + 1;
 
-function keyCheck(event) {
+    if (runImageNumber === 11) {
+        runImageNumber = 1;
+    }
+
+    boy.src = "assest/Run(" + runImageNumber + ").png";
 
 }
+
+function runAnimationStart() {
+    runAnimationNumber = setInterval(runAnimation, 100);
+    clearInterval(idelAnimation);
+
+}
+
 
